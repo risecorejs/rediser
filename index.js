839686660bacb583839b2727b579bc027e35e4be
amenov <amenov.abdulsalam@gmail.com>
2022-07-03 19:31:46 +0600

    fix

--- index.js
+++ index.js
@@ -3,43 +3,44 @@ const models = require('@risecorejs/core/models')
 
 /**
  * REDISER
- * @param redisOptions {Object?}
+ * @param options {Object?}
  * @returns {(function(*, *, *): Promise<any>)|*}
  */
-module.exports = (redisOptions) => async (redisKey, model, options) => {
-  const redisClient = await getRedisClient(redisOptions)
+module.exports = async (options) => {
+  const redisClient = options?.redisClient || (await getRedisClient((redisOptions = options)))
 
-  if (typeof model === 'string') {
-    model = {
-      name: model,
-      method: 'findOne',
-      options: {}
-    }
-  } else {
-    model = {
-      name: model.name,
-      method: model.method || 'findOne',
-      options: model.options || {}
-    }
-  }
-
-  const redisData = await redisClient.get(redisKey)
-
-  if (redisData) {
-    return JSON.parse(redisData)
-  } else {
-    const Model = models[model.name]
-
-    const dbData = await Model[model.method](model.options)
-
-    await redisClient.set(redisKey, JSON.stringify(dbData))
-
-    await redisClient.quit()
-
-    if ((!dbData || !dbData.length) && options?.defaultValue) {
-      return await options.defaultValue()
+  return async (redisKey, model, options) => {
+    if (typeof model === 'string') {
+      model = {
+        name: model,
+        method: 'findOne',
+        options: {}
+      }
+    } else {
+      model = {
+        model: model.model,
+        name: model.name,
+        method: model.method || 'findOne',
+        options: model.options || {}
+      }
     }
 
-    return dbData
+    const _redisClient = options?.redisClient || redisClient
+
+    const redisData = await _redisClient.get(redisKey)
+
+    if (redisData) {
+      return JSON.parse(redisData)
+    } else {
+      const dbData = await (model.model || models[model.name])[model.method](model.options)
+
+      await _redisClient.set(redisKey, JSON.stringify(dbData))
+
+      if ((!dbData || !dbData.length) && options?.defaultValue) {
+        return options.defaultValue()
+      }
+
+      return dbData
+    }
   }
 }
